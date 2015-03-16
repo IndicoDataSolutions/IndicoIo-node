f39a69272a3c63343352167b4ceb5432f3a4ef35
Madison May <madison@indico.io>
2015-03-16 18:41:35 -0400

    Reading configuration from environment variables and configuration files

--- test/images.js
+++ test/images.js
@@ -3,59 +3,51 @@ var indico = require('..')
   , should = require('chai').should();
 
 describe('BatchImage', function () {
-  var username = process.env.INDICO_USERNAME;
-  var password = process.env.INDICO_PASSWORD;
-  if (username && password) {
-    var auth = {
-      "username": username,
-      "password": password
-    }
+  describe('batchFer', function() {
+    it('should get the right response format', function(done) {
+      indico.batchFer([data], function(err, res) {
+      if (err) {
+        done(err);
+        return;
+      }
 
-    describe('batchFer', function() {
-      it('should get the right response format', function(done) {
-        indico.batchFer([data], auth, function(err, res) {
-        if (err) {
-          done(err);
-          return;
-        }
-
-        res.should.have.length(1);
-        Object.keys(res[0]).should.have.length(6);
-        done();
-        });  
-      });
+      res.should.have.length(1);
+      Object.keys(res[0]).should.have.length(6);
+      done();
+      });  
     });
+  });
 
-    describe('batchFacialFeatures', function() {
-      it('should get the right response format', function(done) {
-        indico.batchFacialFeatures([data], auth, function(err, res) {
-        if (err) {
-          done(err);
-          return;
-        }
+  describe('batchFacialFeatures', function() {
+    it('should get the right response format', function(done) {
+      indico.batchFacialFeatures([data], function(err, res) {
+      if (err) {
+        done(err);
+        return;
+      }
 
-        res.should.have.length(1);
-        Object.keys(res[0]).should.have.length(48);
-        done();
-        });  
-      });
+      res.should.have.length(1);
+      Object.keys(res[0]).should.have.length(48);
+      done();
+      });  
     });
+  });
 
-    describe('batchImageFeatures', function() {
-      it('should get the right response format', function(done) {
-        indico.batchImageFeatures([data], auth, function(err, res) {
-        if (err) {
-          done(err);
-          return;
-        }
+  describe('batchImageFeatures', function() {
+    it('should get the right response format', function(done) {
+      indico.batchImageFeatures([data], function(err, res) {
+      if (err) {
+        done(err);
+        return;
+      }
 
-        res.should.have.length(1);
-        Object.keys(res[0]).should.have.length(2048);
-        done();
-        });  
-      });
+      res.should.have.length(1);
+      Object.keys(res[0]).should.have.length(2048);
+      done();
+      });  
     });
-  }
+  });
+
 });
 
 describe('Image', function() {
--- test/text.js
+++ test/text.js
@@ -2,75 +2,67 @@ var indico = require('..');
 var should = require('chai').should();
 
 describe('BatchText', function () {
-  var username = process.env.INDICO_USERNAME;
-  var password = process.env.INDICO_PASSWORD;
-  if (username && password) {
-    var auth = {
-      "username": username,
-      "password": password
-    }
-    describe('batchPolitical', function() {
-      it('should get the right response format', function(done) {
-        indico.batchPolitical(["Guns don't kill people, people kill people."], auth, function(err, res) {
+  describe('batchPolitical', function() {
+    it('should get the right response format', function(done) {
+      indico.batchPolitical(["Guns don't kill people, people kill people."], function(err, res) {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      res.should.have.length(1);
+      Object.keys(res[0]).should.have.length(4);
+      done();
+      });  
+    });
+  });
+
+  describe('batchSentiment', function() {
+    it('should get the right response format', function(done) {
+      indico.batchSentiment(['Really enjoyed the movie.'], function(err, res) {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      res.should.have.length(1);
+      res.should.be.above(0.5);
+      done();
+      });
+    });
+  });
+
+  describe('batchLanguage', function() {
+    it('should get the right response format', function(done) {
+      indico.batchLanguage(['Quis custodiet ipsos custodes'], function(err, res) {
         if (err) {
           done(err);
           return;
         }
 
-        res.should.have.length(1);
-        Object.keys(res[0]).should.have.length(4);
+        res.should.have.length(1)
+        // number of languages
+        Object.keys(res[0]).should.have.length(33)
         done();
-        });  
       });
     });
+  });
 
-    describe('batchSentiment', function() {
-      it('should get the right response format', function(done) {
-        indico.batchSentiment(['Really enjoyed the movie.'], auth, function(err, res) {
+  describe('batchTextTags', function() {
+    it('should get the right response format', function(done) {
+      indico.batchTextTags(['Really enjoyed the movie.'], function(err, res) {
         if (err) {
           done(err);
           return;
         }
 
-        res.should.have.length(1);
-        res.should.be.above(0.5);
+        res.should.have.length(1)
+        // number of categories
+        Object.keys(res[0]).should.have.length(111)
         done();
-        });
       });
     });
-
-    describe('batchLanguage', function() {
-      it('should get the right response format', function(done) {
-        indico.batchLanguage(['Quis custodiet ipsos custodes'], auth, function(err, res) {
-          if (err) {
-            done(err);
-            return;
-          }
-
-          res.should.have.length(1)
-          // number of languages
-          Object.keys(res[0]).should.have.length(33)
-          done();
-        });
-      });
-    });
-
-    describe('batchTextTags', function() {
-      it('should get the right response format', function(done) {
-        indico.batchTextTags(['Really enjoyed the movie.'], auth, function(err, res) {
-          if (err) {
-            done(err);
-            return;
-          }
-
-          res.should.have.length(1)
-          // number of categories
-          Object.keys(res[0]).should.have.length(111)
-          done();
-        });
-      });
-    });
-  }
+  });
 });
 
 describe('Text', function() {
